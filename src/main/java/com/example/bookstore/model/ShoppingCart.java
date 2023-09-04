93c0bb0eb72895ffb8aa254f6e5edcfc1a99111f
irynamatveieva <irina.m2004@icloud.com>
2023-09-04 16:05:52 +0300

    added equals and hashcode methods and validation for dto cartItemRequestDto

--- src/main/java/com/example/bookstore/model/ShoppingCart.java
+++ src/main/java/com/example/bookstore/model/ShoppingCart.java
@@ -8,6 +8,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,4 +26,23 @@ public class ShoppingCart {
     private User user;
     @OneToMany(mappedBy = "shoppingCart")
     private Set<CartItem> cartItems = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ShoppingCart that = (ShoppingCart) o;
+        return Objects.equals(id, that.id)
+                && Objects.equals(user, that.user)
+                && Objects.equals(cartItems, that.cartItems);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, cartItems);
+    }
 }
